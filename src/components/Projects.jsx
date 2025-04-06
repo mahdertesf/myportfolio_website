c2932e8c478cacd7647fa6796704b4a1b9e44f31
Mahder Tesfaye Abebe <mahdertesfaye11@gmail.com>
2025-04-06 23:27:00 +0300

    refactor Projects component to separate special projects and enhance layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 import SectionWrapper from './SectionWrapper';
 
 const Projects = () => {
-  const projects = [
+
+  const Special_Projects = [
     {
-      title: 'Fullstack Little Lemon Restaurant Website',
-      description: 'A complete website for Little Lemon restaurant built with Django, including reservation management and menu display functionalities.',
-      image: '/assets/images/lemon.png', 
-      link: 'https://github.com/mahdertesf/Meta_django_web_framework_final_project'
+      title: 'Fine-Tune Gemma2 models ',
+      description: 'A web app with simple UI to fine tune Gemma2 models for different tasks ',
+      github_link: "https://github.com/mahdertesf/Mahder-AI",
+      image: '/assets/images/special_project/gemma.png',
+      youtube_link:"https://youtu.be/PIFOzK_SzVI?feature=shared"
+  
     },
+    {
+      title:"Telegram Channel  Hate Content Analyzer",
+      description: "A web app to analyze hate content in telegram channels using Transformer Encoders",
+      github_link:"https://github.com/mahdertesf/Mahder-AI",
+      image:"/assets/images/special_project/hate.png",
+      youtube_link:"https://youtu.be/VUNmOHDsebY?si=qDUwXu134sNOqSts"
+      
+    }
+
+
+
+  ]
+
+
+
+  const projects = [
+   
     {
       title: 'Landing Page for a Virtual Reality Platform',
       description: 'A landing page for a Virtual Reality platform designed to attract users with a modern and engaging interface and interactive elements.',
@@ -99,17 +119,42 @@ const Projects = () => {
   return (
     <SectionWrapper id="projects">
       <h2 className="text-3xl font-bold mb-8 text-center animate-fadeIn">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((project, index) => (
-          <div key={index} className="border border-glow p-4 rounded-lg shadow-md animate-glowBorder relative transition-all duration-300 group">
-            <div className="overflow-hidden relative pb-[56.25%] rounded-lg  group-hover:scale-105 transition-transform duration-300">
-              <img src={project.image} alt={project.title} className="object-contain absolute top-0 left-0 w-full h-full" />
+      <div className='flex flex-col items-center justify-center'>
+      
+        <div className='flex flex-col w-full items-center justify-center gap-10 mb-12'>   
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
+          {Special_Projects.map((project, index) => (
+            <div key={index} className="border border-glow w-full flex flex-col p-4 rounded-lg shadow-md animate-glowBorder relative transition-all duration-300 group">
+              <div className=" w-full relative pb-[56.25%] rounded-lg overflow-hidden group-hover:scale-105 transition-transform duration-300">
+                <img src={project.image} alt={project.title} className="object-cover absolute top-0 left-0 w-full h-full" />
+              </div>
+              <div>
+                <h3 className="text-xl font-semibold mt-4 mb-2 text-glow animate-fadeIn">{project.title}</h3>
+                <p className="text-light animate-fadeIn ">{project.description}</p>
+                <div className='flex flex-col mt-4'>
+                  <a href={project.github_link} className='mb-2 inline-block text-primary hover:text-glow border-b border-primary transition-colors duration-300 animate-fadeIn'>See Project on GitHub</a>
+                  <a href={project.youtube_link} className='inline-block text-primary hover:text-glow border-b border-primary transition-colors duration-300 animate-fadeIn'>Watch on YouTube</a>
+                </div>
+              </div>
             </div>
-            <h3 className="text-xl font-semibold mt-4 mb-2 text-glow animate-fadeIn">{project.title}</h3>
-            <p className="text-light animate-fadeIn ">{project.description}</p>
-              <a href={project.link} className=' mt-3 inline-block text-primary hover:text-glow border-b border-primary  transition-colors duration-300 animate-fadeIn'>See Project</a>
+          ))}
           </div>
-        ))}
+        </div>
+        
+        <h3 className="text-2xl font-semibold mb-4 text-center text-glow animate-fadeIn">Other Projects</h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+          {projects.map((project, index) => (
+            <div key={index} className="border border-glow p-4 rounded-lg shadow-md animate-glowBorder relative transition-all duration-300 group">
+              <div className="overflow-hidden relative pb-[56.25%] rounded-lg  group-hover:scale-105 transition-transform duration-300">
+                <img src={project.image} alt={project.title} className="object-cover absolute top-0 left-0 w-full h-full" />
+              </div>
+              <h3 className="text-xl font-semibold mt-4 mb-2 text-glow animate-fadeIn">{project.title}</h3>
+              <p className="text-light animate-fadeIn">{project.description}</p>
+                <a href={project.link} className=' mt-3 inline-block text-primary hover:text-glow border-b border-primary  transition-colors duration-300 animate-fadeIn'>See Project</a>
+            </div>
+          ))}
+        </div>
+
       </div>
     </SectionWrapper>
   );
